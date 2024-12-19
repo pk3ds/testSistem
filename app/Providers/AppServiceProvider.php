90470e961f48f4ef6a7613f2fec41eea2fbea583
aiman <aiman.azhari@tm.com.my>
2024-12-19 17:36:58 +0800

    adding production files that will capture https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Ensure Laravel generates HTTPS URLs in production
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
+        // Set Vite prefetch concurrency
         Vite::prefetch(concurrency: 3);
     }
 }
